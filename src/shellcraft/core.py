b6f3715e784dd917d1b7580af85c59a0a23e37f5
Manuel Ebert <manuel@1450.me>
2017-05-25 07:43:59 -0700

    Docs and requirements

--- src/shellcraft/core.py
+++ src/shellcraft/core.py
@@ -12,13 +12,23 @@ RESOURCES = ['clay', 'energy', 'ore']
 
 
 class ResourceProxy(object):
-    """Countable Resources."""
+    """Proxy for accessing Resource messages in a GameState."""
 
     def __init__(self, field):
+        """Initialise the Proxy.
+
+        Args:
+            field (shellcraft.game_state_pb2.Resources): Resource message.
+        """
         self._resources = field
 
     def add(self, resource, value):
-        """Add resource."""
+        """Add resource.
+
+        Args:
+            resource (str)
+            value (float)
+        """
         setattr(self._resources, resource, getattr(self._resources, resource, 0) + value)
 
     def get(self, resource, default=0):
@@ -31,22 +41,43 @@ class ResourceProxy(object):
 
 
 class ItemProxy(object):
+    """Proxy for accessing serializable items."""
+
     def __init__(self, field, factory):
+        """Initialise the Proxy.
+
+        Args:
+            field: Message of 'Repeated' type
+            factory (shellcraft.core.BaseFactory): Factory used to produce the item.
+        """
         self._field = field
         self._factory = factory
         self._items = [factory.make(pb) for pb in field]
 
     def __iter__(self):
+        """Iterate over items."""
         return self._items.__iter__()
 
     def remove(self, item):
+        """Remove an item."""
         self._items.remove(item)
         self._field.remove(item._pb)
 
     def __bool__(self):
+        """True if the proxy contains any items."""
         return bool(self._items)
 
     def add(self, item):
+        """Add a new item.
+
+        This is achieved by first adding a Protobuf message to the field,
+        then using the factory to make a new item from a given template,
+        setting all fields on the Protobuf message and attaching it to the
+        newly generated item.
+
+        Args:
+            item: Item name or Item instance to copy from
+        """
         pb = self._field.add()
         new_item = self._factory.make(item)
         for field in self._factory.PB_CLASS.DESCRIPTOR.fields_by_name.keys():
@@ -58,9 +89,13 @@ class ItemProxy(object):
 
 
 class BaseItem(object):
+    """Abstract class for new items."""
+
     _pb = None
+    """This links the item to a serializable Protobuf message that is part of the GameState."""
 
     def __init__(self, name):
+        """Initialise an empty item."""
         self.name = name
         self.difficulty = 0
         self.description = ""
@@ -88,26 +123,36 @@ class BaseItem(object):
         return item
 
     def __repr__(self):
+        """String representation of the item."""
         return self.name
 
     def __setattr__(self, key, value):
+        """Override attribute setter for items with attached Protobuf message."""
         if key != "_pb" and self._pb and key in self._pb.__class__.DESCRIPTOR.fields_by_name.keys():
             setattr(self._pb, key, value)
         else:
             self.__dict__[key] = value
 
     def __getattr__(self, key):
+        """Override attribute getter for items with attached Protobuf message."""
         if key != "_pb" and self._pb and key in self._pb.__class__.DESCRIPTOR.fields_by_name.keys():
             return getattr(self._pb, key)
         raise AttributeError(key)
 
 
 class BaseFactory(object):
+    """Factory pattern to instantiate items."""
+
     FIXTURES = "collection.yaml"
     ITEM_CLASS = BaseItem
     PB_CLASS = None
 
     def __init__(self, game):
+        """Initialise the Factory.
+
+        Args:
+            game (shellcraft.shellcraft.Game): Game object.
+        """
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", self.FIXTURES)) as f:
             self.all_items = {name: self.ITEM_CLASS.from_dict(name, data) for name, data in yaml.load(f).items()}
         self.game = game
@@ -119,6 +164,14 @@ class BaseFactory(object):
         return self.all_items.get(item_name)
 
     def make(self, source):
+        """Create a new unique item from a source.
+
+        The source may be a string, in which case the item is created from
+        the item library defined in a YAML file, or it may be a Protobuf message,
+        which is typically the case when loading a saved game and instantiating
+        serialized items (such as tools or missions), or it may be another Item
+        instance which will be copied.
+        """
         if isinstance(source, str):
             return copy(self.get(source))
         elif self.PB_CLASS and isinstance(source, self.PB_CLASS):
@@ -147,6 +200,7 @@ class BaseFactory(object):
         return True
 
     def apply_effects(self, item_name):
+        """Apply all effects of an item."""
         item = self.get(item_name)
 
         # Enable commands

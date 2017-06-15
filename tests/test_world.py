0392602070079e5e8563c2e322d509328f7b695b
Manuel Ebert <manuel@1450.me>
2017-06-14 17:06:53 -0700

    Fix tests

--- tests/test_world.py
+++ tests/test_world.py
@@ -9,15 +9,13 @@ Tests for `shellcraft` module.
 
 from __future__ import unicode_literals
 
-import pytest
-from click.testing import CliRunner
-from shellcraft.world import NPC
+from shellcraft.world import NPCFactory
+from shellcraft.utils import format_name
 
 
 def test_npc_generation():
     """Test that we can generate random NPCs."""
-    npc = NPC.generate()
-    assert npc.name
-    npc2 = NPC.generate()
-    assert npc2.name != npc.name
-
+    npc = NPCFactory.make()
+    assert format_name(npc)
+    npc2 = NPCFactory.make()
+    assert format_name(npc2) != format_name(npc)
